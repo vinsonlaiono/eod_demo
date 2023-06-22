4fa1cfc7f0cd28671853ad201a271787ce6dc064
vinsonlaiono <vinson.aiono@gmail.com>
2023-06-22 15:55:48 -0600

    finished dowhile example

---
+++ index.js
@@ -0,0 +1,19 @@
+// do while loop
+// for loop
+// while loop
+
+// for (let i = 0; i < 10; i++) {}
+
+// let i = 0;
+// while (i < 10) {
+//     i++;
+// }
+
+let res = '';
+let x = 1;
+do {
+    res += x;
+    x++;
+} while (x < 6);
+
+console.log(res);
